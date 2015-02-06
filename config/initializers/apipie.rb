e70caab820e8d0c93850afb6fa91433637660df4
rockholla <patrickforce@gmail.com>
2015-02-05 20:51:19 -0700

    try new apipie matcher

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -3,7 +3,7 @@ Apipie.configure do |config|
   config.api_base_url            = "/api/v1"
   config.doc_base_url            = "/api/v1/docs"
   # where is your API defined?
-  config.api_controllers_matcher = "#{Rails.root}/app/controllers/api/v1/*.rb"
+  config.api_controllers_matcher = "#{Rails.root}/app/controllers/api/**/*.rb"
   config.app_info                = "The Pokering HTTP API is a RESTful JSON API designed to provide an data interface for any and all Pokering client applications."
   config.markup                   = Apipie::Markup::Markdown.new
 end
